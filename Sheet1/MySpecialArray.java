d7fad18e42841575f4b07ad723f0ff09d390d810
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-03-01 16:45:44 +0200

    q1)b.

--- Sheet1/MySpecialArray.java
+++ Sheet1/MySpecialArray.java
@@ -14,12 +14,31 @@ public class MySpecialArray {
             arr[arr.length-1-i]=temp;
         }        
     }
+    public static int[] sumEvenOdd(int[] arr)
+    {int [] a=new int[2];
+       a[0]=0;
+       a[1]=0;     
+        if(arr==null)
+        return a;
+
+        else 
+        {
+            for(int i=0;i<arr.length;i++)
+            {
+                if(arr[i]%2==0)a[0]+=arr[i];
+                else a[1]+=arr[i];
+            }
+            return a;
+        }
+
+    } 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
         n=sc.nextInt();
         int [] arr=new int[n];
+        
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=sc.nextInt();
@@ -29,6 +48,10 @@ public class MySpecialArray {
         {
             System.out.println(arr[i]);
         }
+        int [] sumEvenOddarr=new int [2];
+        sumEvenOddarr=sumEvenOdd(arr);
+        System.out.println(sumEvenOddarr[0]);
+        System.out.println(sumEvenOddarr[1]);
 
     }
 }
