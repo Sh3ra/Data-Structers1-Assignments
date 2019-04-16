682f963cb41a0db7b129993bb6c485f152dacc85
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-04-16 15:13:22 +0200

    single list class

--- assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/SinglyLinkedList.java
+++ assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/SinglyLinkedList.java
@@ -1,4 +1,54 @@
 package eg.edu.alexu.csd.datastructure.linkedList;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList implements ILinkedList {
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, Object element) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ILinkedList sublist(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
 }
