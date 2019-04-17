eafc227c44dea4e4bf26d9fd4bec9ab231ef7b1e
MarwanAshrafGouda <45739389+MarwanAshrafGouda@users.noreply.github.com>
2019-04-17 17:33:54 +0200

    kjjhg

--- assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/Main.java
+++ assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/Main.java
@@ -3,7 +3,16 @@ package eg.edu.alexu.csd.datastructure.linkedList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("");
+    	DoublyLinkedList test = new DoublyLinkedList();
+    	test.add(5);
+    	test.add(0);
+    	test.add(5555);
+    	test.add(0, 90);
+    	DoublyLinkedList.DLNode temp;
+    	temp = test.head;
+    	while(temp!=null) {
+    		System.out.println(temp.value);
+    		temp=temp.next;
+    	}
     }
 }
