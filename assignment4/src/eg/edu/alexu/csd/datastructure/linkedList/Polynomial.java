60d6026d122c858979d53dce0675b949f37fdc4b
MarwanAshrafGouda <45739389+MarwanAshrafGouda@users.noreply.github.com>
2019-04-18 09:23:17 +0200

    added poly print

--- assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/Polynomial.java
+++ assignment4/src/eg/edu/alexu/csd/datastructure/linkedList/Polynomial.java
@@ -2,14 +2,46 @@ package eg.edu.alexu.csd.datastructure.linkedList;
 
 public class Polynomial extends SinglyLinkedList implements IPolynomialSolver {
 	
+	static Polynomial A = new Polynomial();
+	static Polynomial B = new Polynomial();
+	static Polynomial C = new Polynomial();
+	static Polynomial R = new Polynomial();
+	
 	public void setPolynomial(char poly, int[][] terms) {
 		
 	}
 
 	
 	public String print(char poly) {
+		Polynomial temp = new Polynomial();
+		switch (poly) {
+		case 'A':
+			temp.head = A.head;
+			break;
+		case 'B':
+			temp.head = B.head;
+			break;
+		case 'C':
+			temp.head = C.head;
+			break;
+		}
 		
-		return null;
+		String polynomial = new String();
+		
+		for(int index = 0; index < temp.size(); index++) {
+			if(temp.get(index) != (Object)0) {
+				if (index == 0)
+					polynomial = temp.get(index) + polynomial;
+				else if (index == 1)
+					polynomial = "X" + polynomial;
+				else 
+					polynomial = temp.get(index) + "X^" + index + polynomial;
+				polynomial = "+" + polynomial;
+			}
+		}
+		if(polynomial.length()>0)
+			polynomial = polynomial.substring(1);
+		return polynomial;
 	}
 
 	
@@ -41,6 +73,9 @@ public class Polynomial extends SinglyLinkedList implements IPolynomialSolver {
 		
 		return null;
 	}
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
 
