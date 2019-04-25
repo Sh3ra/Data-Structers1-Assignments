70903ff15630c513984cb0e7e014c51d0b3d14f9
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-04-25 19:48:21 +0200

    laying bases

---
+++ Assignment5/src/eg/edu/alexu/csd/datastructure/linkedList/Stack.java
@@ -0,0 +1,28 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+
+public class Stack implements IStack {
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void push(Object element) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
