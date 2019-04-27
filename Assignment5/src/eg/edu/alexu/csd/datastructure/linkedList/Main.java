ac07232c4bed17bfe26cb96051a979eb4831e77d
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-04-27 11:52:02 +0200

    Stack class is done

--- Assignment5/src/eg/edu/alexu/csd/datastructure/linkedList/Main.java
+++ Assignment5/src/eg/edu/alexu/csd/datastructure/linkedList/Main.java
@@ -3,13 +3,7 @@ package eg.edu.alexu.csd.datastructure.linkedList;
 public class Main {
 
     public static void main(String[] args) {
-    Stack test=new Stack();
-    test.push(1);
-    test.push('x');
-    test.push("youssef Cool");
-    System.out.println(test.pop());
-    System.out.println(test.peek());
-    System.out.println(test.peek());
-    System.out.println(test.pop());
+
     }
+
 }
